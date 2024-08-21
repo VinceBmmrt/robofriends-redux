950238efc7061d33ac92e828527773a86ebab654
VinceBmmrt <vincebommert75@gmail.com>
2024-08-21 18:46:13 +0200

    Basic test for actions

---
+++ src/actions.test.js
@@ -0,0 +1,26 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import * as actions from "./actions";
+import * as types from "./constants";
+
+export const mockStore = configureMockStore([thunk]);
+
+describe("actions", () => {
+  it("should create an action to search", () => {
+    const text = "Finish docs";
+    const expectedAction = {
+      type: types.CHANGE_SEARCHFIELD,
+      payload: text,
+    };
+    expect(actions.setSearchField(text)).toEqual(expectedAction);
+  });
+});
+
+describe("Fetch robots action PENDING", () => {
+  it("handles changing a purchase status and fetches all purchases", () => {
+    const store = mockStore();
+    store.dispatch(actions.requestRobots());
+    const action = store.getActions();
+    expect(action[0]).toEqual({ type: "REQUEST_ROBOTS_PENDING" });
+  });
+});
